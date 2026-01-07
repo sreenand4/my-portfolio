463386614b71625684ccb9b7817ac1736121bdb2
sreenandm <148009692+sreenandm@users.noreply.github.com>
2026-01-07 14:21:56 -0500

    Configure configuration for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
+  site: 'https://sreenand4.github.io',
+  base: '/my-portfolio',
   vite: {
     plugins: [tailwindcss()],
     ssr: {
